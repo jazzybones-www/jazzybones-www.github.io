7d88a5c53fd9899f8e930381665d2e7eab7ace77
jazzybones <jazzybones@proton.me>
2024-07-12 13:54:33 -0500

    remove intermediate files

--- root/make-build-rule.sh
+++ root/make-build-rule.sh
@@ -11,5 +11,4 @@ deps_cmd="$(grep '^\\jb_dependencies:' "$in_file" | perl -pe 's|^.*?:||')"
 cat > $rule_file << --EOF--
 $out_file: $in_file $deps_cmd
 	./parse-jb.sh < \$< > \$@
-	rm $rule_file
 --EOF--
