ddbf69182a28549dbf8b3a921b2fe635caf5c46b
jazzybones <jazzybones@proton.me>
2024-07-13 15:43:05 -0500

    timestamp status

--- root/status.sh
+++ root/status.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 git log --pretty=oneline status.txt | awk '{print $1;}' | while read commit ; do
-	git revert "$commit" status.txt
+	git checkout "$commit" ./status.txt > /dev/null 2>&1
+	printf "%s: " "$(git show -s --format=%ad --date iso8601 "$commit")"
 	cat status.txt
 	echo
 done
 
-git revert HEAD status.txt
+git checkout HEAD ./status.txt > /dev/null 2>&1
