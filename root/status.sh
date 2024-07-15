88df2e973fa58cb84a84defde558a0873099b977
jazzybones <jazzybones@proton.me>
2024-07-15 18:56:30 -0500

    change status format

--- root/status.sh
+++ root/status.sh
@@ -2,7 +2,7 @@
 
 git log --pretty=oneline status.txt | awk '{print $1;}' | while read commit ; do
 	git checkout "$commit" ./status.txt > /dev/null 2>&1
-	printf "%s: " "$(git show -s --format=%ad --date iso8601 "$commit")"
+	printf "## %s\n\n" "$(git show -s --format=%ad --date iso8601 "$commit")"
 	cat status.txt
 	echo
 done
